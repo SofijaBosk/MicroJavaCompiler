43e8d88a2021e435b5e2119657ff5261ca03d5a1
Софија Бошковић <sofija98733@gmail.com>
2023-12-05 12:33:36 +0100

    Init and Lexer

---
+++ MJCompiler/src/rs/ac/bg/etf/pp1/sym.java
@@ -0,0 +1,60 @@
+package rs.ac.bg.etf.pp1;
+
+public class sym {
+	  /* terminals */
+	  public static final int ELSE = 17;
+	  public static final int IDENT = 15;
+	  public static final int PLUS = 11;
+	  public static final int RBRACE = 4;
+	  public static final int RPAREN = 7;
+	  public static final int EQUAL = 12;
+	  public static final int SEMI = 5;
+	  public static final int NUMBER= 14;
+	  public static final int RETURN = 10;
+	  public static final int IF = 16;
+	  public static final int LPAREN = 6;
+	  public static final int LBRACE = 3;
+	  public static final int COMMA = 8;
+	  public static final int EOF = 0;
+	  public static final int PROG = 2;
+	  public static final int PRINT = 9;
+	  public static final int error = 1;
+	  public static final int VOID = 13;
+	  
+	  //CONST
+	  public static final int NUM_CONST = 18;
+	  public static final int CHAR_CONST = 19;
+	  public static final int BOOL_CONST = 20;
+	  
+	  public static final int CLASS=21;
+	  public static final int WHILE = 22;
+	  public static final int BREAK = 23;
+	  public static final int CONTINUE=24;
+	  public static final int READ=25;
+	  public static final int MINUS=26;
+	  public static final int MUL=27;
+	  public static final int DIV=28;
+	  public static final int MOD=29;
+	  public static final int BOOL_EQUAL=30;
+	  public static final int BOOL_NOT_EQUAL=31;
+	  public static final int GREATER=32;
+	  public static final int GREATER_OR_EQUAL=33;
+	  public static final int LESS=34;
+	  public static final int LESS_OR_EQUAL=35;
+	  public static final int LSQUARE=36;
+	  public static final int RSQUARE=37;
+	  
+	  public static final int CONST=38;
+	  public static final int FOREACH=39;
+	  public static final int EXTENDS=40;
+	  
+	  public static final int AND=41;
+	  public static final int OR=42;
+	  public static final int EQUALS=43;
+	  public static final int INC=44;
+	  public static final int DEC=45;
+	  public static final int COLON=46;
+	  public static final int ASSIGN=47;
+	  public static final int DOT=48;
+
+}
