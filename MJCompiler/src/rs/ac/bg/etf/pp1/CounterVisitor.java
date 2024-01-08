c4895d90ada65fb410167a6a09f0ab33d3fde6fd
unknown <sofija98733@gmail.com>
2024-01-08 14:10:05 +0100

    Code generation: skelet code

---
+++ MJCompiler/src/rs/ac/bg/etf/pp1/CounterVisitor.java
@@ -0,0 +1,29 @@
+package rs.ac.bg.etf.pp1;
+
+import rs.ac.bg.etf.pp1.ast.FormParDecl_Single;
+import rs.ac.bg.etf.pp1.ast.VarDecl;
+import rs.ac.bg.etf.pp1.ast.VisitorAdaptor;
+
+public class CounterVisitor extends VisitorAdaptor {
+	
+	protected int countVar;
+	
+	public int getCountVar() {
+		return countVar;
+	}
+	
+	public static class FormParamCounter extends CounterVisitor {
+
+		@Override
+		public void visit(FormParDecl_Single formPar) {
+			countVar++;
+		}		
+	}
+	
+	public static class VarCounter extends CounterVisitor {		
+		@Override
+		public void visit(VarDecl VarDecl) {
+			countVar++;
+		}
+	}
+}
